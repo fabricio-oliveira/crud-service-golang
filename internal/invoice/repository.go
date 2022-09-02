338aa65299c86f80953b632281d5c1c2a048eb43
Fabricio Oliveira <fabricioque@gmail.com>
2022-09-01 21:39:18 -0300

    dynamoDB improvements

--- internal/invoice/repository.go
+++ internal/invoice/repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -18,27 +19,35 @@ var TABLE_NAME = "invoice"
 var PROJECTION_EXPRESSION = "Id, Date, BillTo, Item, CreatedAt, UpdatedAt"
 
 func getClient() *dynamodb.Client {
-	customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-		if service == dynamodb.ServiceID {
-			return aws.Endpoint{
-				PartitionID:   "aws",
-				URL:           util.Getenv("AWS_DYNAMODB_ENDPOINT", "http://localhost:8000"),
-				SigningRegion: util.Getenv("AWS_REGION", "us-est-1"),
-			}, nil
+
+	var properties = []func(*config.LoadOptions) error{}
+	if util.Getenv("APP_ENV", "dev") == "dev" {
+		customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+			if service == dynamodb.ServiceID {
+				return aws.Endpoint{
+					PartitionID:   "aws",
+					URL:           util.Getenv("AWS_DYNAMODB_ENDPOINT", "http://localhost:8000"),
+					SigningRegion: util.Getenv("AWS_REGION", "us-est-1"),
+				}, nil
+			}
+			// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
+			return aws.Endpoint{}, fmt.Errorf("unknown endpoint requested")
+		})
+		customCredentials := credentials.StaticCredentialsProvider{
+			Value: aws.Credentials{
+				AccessKeyID:     util.Getenv("AWS_ACCESS_KEY_ID", "DUMMY"),
+				SecretAccessKey: util.Getenv("AWS_SECRET_ACCESS_KEY", "DUMMY"),
+				Source:          "Hard-coded credentials; values are irrelevant for local DynamoDB",
+			}}
+		properties = [](func(*config.LoadOptions) error){
+			config.WithEndpointResolverWithOptions(customResolver),
+			config.WithCredentialsProvider(customCredentials),
 		}
-		// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
-		return aws.Endpoint{}, fmt.Errorf("unknown endpoint requested")
-	})
+	}
 
 	cfg, err := config.LoadDefaultConfig(
 		context.TODO(),
-		config.WithEndpointResolverWithOptions(customResolver),
-		config.WithCredentialsProvider(credentials.StaticCredentialsProvider{
-			Value: aws.Credentials{
-				AccessKeyID: "dummy", SecretAccessKey: "dummy", SessionToken: "dummy",
-				Source: "Hard-coded credentials; values are irrelevant for local DynamoDB",
-			}}),
-	)
+		properties...)
 
 	if err != nil {
 		return nil
@@ -60,6 +69,7 @@ func getInvoice(id int) *dynamodb.GetItemInput {
 
 func createInvoice(invoice *Invoice) error {
 	client := getClient()
+	setDate(invoice)
 	item, err := attributevalue.MarshalMap(invoice)
 	if err != nil {
 		return err
@@ -74,4 +84,14 @@ func createInvoice(invoice *Invoice) error {
 		return err
 	}
 	return nil
+}
+
+func setDate(invoice *Invoice) {
+	if invoice.CreatedAt == "" {
+		invoice.CreatedAt = time.Now().String()
+	}
+
+	if invoice.UpdateAt == "" {
+		invoice.UpdateAt = time.Now().String()
+	}
 }

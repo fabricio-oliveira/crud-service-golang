16653e6f165f04eb77f15a1469fc1dc6f3f24263
Fabricio Oliveira <fabricioque@gmail.com>
2022-09-13 10:31:06 -0300

    add more tests

--- internal/repository/dynamodb/operations_test.go
+++ internal/repository/dynamodb/operations_test.go
@@ -2,6 +2,7 @@ package dynamoDB
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -66,6 +67,44 @@ func TestGet(t *testing.T) {
 	)
 }
 
+func TestGetError(t *testing.T) {
+	// inputs
+	projection := "Id, TestColumn"
+	key := map[string]string{
+		"Id": "1",
+	}
+
+	// mock
+	mock := &dynamodb_mock.DynamoDBMOCK{
+		MockError: fmt.Errorf("internal error"),
+	}
+
+	patchGuard := sm.Patch(getClient, func() DynamoDBAPI {
+		return mock
+	})
+	defer patchGuard.Unpatch()
+
+	v, err := Get[TestRecord](tableName, projection, key)
+
+	assert.Error(t, err)
+	assert.Nil(t, v)
+
+	assert.Equal(t,
+		&dynamodb_mock.SpyParams[dynamodb.GetItemInput]{
+			Ctx: context.TODO(),
+			Params: &dynamodb.GetItemInput{
+				Key: map[string]types.AttributeValue{
+					"Id": &types.AttributeValueMemberS{Value: "1"},
+				},
+				TableName:            &tableName,
+				ConsistentRead:       aws.Bool(true),
+				ProjectionExpression: &projection,
+			},
+		},
+		mock.SpyGetParams,
+	)
+}
+
 func TestGetAll(t *testing.T) {
 	// mock
 	mock := &dynamodb_mock.DynamoDBMOCK{
@@ -109,6 +148,33 @@ func TestGetAll(t *testing.T) {
 	)
 }
 
+func TestGetAllError(t *testing.T) {
+	// mock
+	mock := &dynamodb_mock.DynamoDBMOCK{
+		MockError: fmt.Errorf("internal error"),
+	}
+
+	patchGuard := sm.Patch(getClient, func() DynamoDBAPI {
+		return mock
+	})
+	defer patchGuard.Unpatch()
+
+	v, err := GetAll[TestRecord](tableName)
+
+	assert.Error(t, err)
+	assert.Nil(t, v)
+
+	assert.Equal(t,
+		&dynamodb_mock.SpyParams[dynamodb.ScanInput]{
+			Ctx: context.TODO(),
+			Params: &dynamodb.ScanInput{
+				TableName: &tableName,
+			},
+		},
+		mock.SpyScanParams,
+	)
+}
+
 func TestCreate(t *testing.T) {
 	// inputs
 	object := TestRecord{
@@ -129,6 +195,43 @@ func TestCreate(t *testing.T) {
 	err := Create(tableName, object)
 
 	assert.NoError(t, err)
+
+	assert.Equal(t,
+		&dynamodb_mock.SpyParams[dynamodb.PutItemInput]{
+			Ctx: context.TODO(),
+			Params: &dynamodb.PutItemInput{
+				TableName: &tableName,
+				Item: map[string]types.AttributeValue{
+					"Id":         &types.AttributeValueMemberS{Value: object.Id},
+					"TestColumn": &types.AttributeValueMemberS{Value: object.TestColumn},
+				},
+				ConditionExpression: aws.String("attribute_not_exists(Id)"),
+			},
+		},
+		mock.SpyPutParams,
+	)
+}
+
+func TestCreateError(t *testing.T) {
+	// inputs
+	object := TestRecord{
+		Id:         "1",
+		TestColumn: "Fake Value",
+	}
+
+	// mock
+	mock := &dynamodb_mock.DynamoDBMOCK{
+		MockError: fmt.Errorf("internal error"),
+	}
+
+	patchGuard := sm.Patch(getClient, func() DynamoDBAPI {
+		return mock
+	})
+	defer patchGuard.Unpatch()
+
+	err := Create(tableName, object)
+
+	assert.Error(t, err)
 
 	assert.Equal(t,
 		&dynamodb_mock.SpyParams[dynamodb.PutItemInput]{
@@ -183,6 +286,43 @@ func TestUpdate(t *testing.T) {
 	)
 }
 
+func TestUpdateError(t *testing.T) {
+	// inputs
+	object := TestRecord{
+		Id:         "1",
+		TestColumn: "Fake Value",
+	}
+
+	// mock
+	mock := &dynamodb_mock.DynamoDBMOCK{
+		MockError: fmt.Errorf("internal error"),
+	}
+
+	patchGuard := sm.Patch(getClient, func() DynamoDBAPI {
+		return mock
+	})
+	defer patchGuard.Unpatch()
+
+	err := Update(tableName, object)
+
+	assert.Error(t, err)
+
+	assert.Equal(t,
+		&dynamodb_mock.SpyParams[dynamodb.PutItemInput]{
+			Ctx: context.TODO(),
+			Params: &dynamodb.PutItemInput{
+				TableName: &tableName,
+				Item: map[string]types.AttributeValue{
+					"Id":         &types.AttributeValueMemberS{Value: object.Id},
+					"TestColumn": &types.AttributeValueMemberS{Value: object.TestColumn},
+				},
+				ConditionExpression: nil,
+			},
+		},
+		mock.SpyPutParams,
+	)
+}
+
 func TestDelete(t *testing.T) {
 	// inputs
 	condition := map[string]string{
@@ -202,6 +342,40 @@ func TestDelete(t *testing.T) {
 	err := Delete(tableName, condition)
 
 	assert.NoError(t, err)
+
+	assert.Equal(t,
+		&dynamodb_mock.SpyParams[dynamodb.DeleteItemInput]{
+			Ctx: context.TODO(),
+			Params: &dynamodb.DeleteItemInput{
+				TableName: &tableName,
+				Key: map[string]types.AttributeValue{
+					"Id": &types.AttributeValueMemberS{Value: "1"},
+				},
+			},
+		},
+		mock.SpyDeleteParams,
+	)
+}
+
+func TestDeleteError(t *testing.T) {
+	// inputs
+	condition := map[string]string{
+		"Id": "1",
+	}
+
+	// mock
+	mock := &dynamodb_mock.DynamoDBMOCK{
+		MockError: fmt.Errorf("internal error"),
+	}
+
+	patchGuard := sm.Patch(getClient, func() DynamoDBAPI {
+		return mock
+	})
+	defer patchGuard.Unpatch()
+
+	err := Delete(tableName, condition)
+
+	assert.Error(t, err)
 
 	assert.Equal(t,
 		&dynamodb_mock.SpyParams[dynamodb.DeleteItemInput]{
